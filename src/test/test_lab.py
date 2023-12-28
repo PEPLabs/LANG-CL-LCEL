67d7eccc54035724cfbde1da7894a7233937ec00
JesterCharles <JesterCharles@gmail.com>
2023-12-28 09:42:22 -0500

    Updated the testing suite to work with the new util file contain the functionality for testing the llm status & relevancy. This also includes an update to the sanity_check in our test suite.

--- src/test/test_lab.py
+++ src/test/test_lab.py
@@ -12,24 +12,28 @@ from langchain.schema.output_parser import StrOutputParser
 from langchain_community.chat_models import ChatHuggingFace
 from langchain_core.prompts import PromptTemplate
 from src.main.lab import get_basic_chain, basic_chain_invoke
+from src.utilities.llm_testing_util import classify_relevancy, llm_wakeup, llm_connection_check
 
 
 class TestLLMResponse(unittest.TestCase):
-    """
-    This test will verify that the connection to an external LLM is made. If it does not
-    work, this may be because the API key is invalid, or the service may be down.
-    If that is the case, this lab may not be completable.
-    """
 
     def test_llm_sanity_check(self):
-        llm = HuggingFaceEndpoint(
-        endpoint_url=os.environ['HF_ENDPOINT'],
-        huggingfacehub_api_token=os.environ['HF_TOKEN'],
-        task="text-generation",
-        model_kwargs={
-            "max_new_tokens": 1024
-        }
-    )
+        """
+        This function is a sanity check for the Language Learning Model (LLM) connection.
+        It attempts to generate a response from the LLM. If a 'Bad Gateway' error is encountered,
+        it initiates the LLM wake-up process. This function is critical for ensuring the LLM is
+        operational before running tests and should not be modified without understanding the
+        implications.
+        Raises:
+            Exception: If any error other than 'Bad Gateway' is encountered, it is raised to the caller.
+        """
+        try:
+            response = llm_connection_check()
+            print(response)
+        except Exception as e:
+            if 'Bad Gateway' in str(e):
+                llm_wakeup()
+                self.fail("LLM is not awake. Please try again in 3-5 minutes.")
 
     """
     The variable returned from the lab function should be an langchain AI response. If this test
@@ -45,38 +49,7 @@ class TestLLMResponse(unittest.TestCase):
         result = basic_chain_invoke("honey bees")
         self.assertIsInstance(result, str)
         self.assertTrue(classify_relevancy(result, "Can you tell me about honey bees?"))
-    
-   
-def classify_relevancy(message, question):
-    prompt_template = PromptTemplate.from_template(
-        """
-        <|system|>
-        You are a chatbot who determines if a given message properly answers a question by replying "yes" or "no".</s>
-        <|user|>
-        Does the following message answer the question: {question}? message: {message}</s>
-        <|assistant|>
-        """
-    )
 
-    model = HuggingFaceEndpoint(
-        endpoint_url="https://z8dvl7fzhxxcybd8.eu-west-1.aws.endpoints.huggingface.cloud",
-        huggingfacehub_api_token="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",
-        task="text-generation",
-        model_kwargs={
-            "max_new_tokens": 200
-        }
-    )
-    chat_model = ChatHuggingFace(llm=model)
-    chain = prompt_template | chat_model | StrOutputParser()
-    result = chain.invoke({"message": message, "question": question})
-    print("Result: " + result)
-    print(message)
-    print(question)
-    if ("yes" in result.lower()):
-        return True
-    else:
-        print(message)
-        return False
 
 if __name__ == '__main__':
     unittest.main()
